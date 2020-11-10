f074822c36d4e4b4d19de3379d41d4c32c85eb7f
CubeConflict <vod-k@live.be>
2020-11-10 18:42:47 +0100

    pas de launcher via steam

---
+++ Cube Conflict.bat
@@ -0,0 +1,14 @@
+@ECHO OFF
+
+set CC_BIN=bin
+
+IF EXIST bin64\cubeconflict.exe (
+    IF /I "%PROCESSOR_ARCHITECTURE%" == "amd64" (
+        set CC_BIN=bin64
+    )
+    IF /I "%PROCESSOR_ARCHITEW6432%" == "amd64" (
+        set CC_BIN=bin64
+    )
+)
+
+start %CC_BIN%\cubeconflict.exe "-u$HOME\My Games\Cube Conflict" -glog.txt %*
