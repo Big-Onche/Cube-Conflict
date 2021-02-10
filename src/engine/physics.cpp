0ea6952410fce6af81c293e536608e396cea12ce
CubeConflict <vod-k@live.be>
2021-02-10 21:22:08 +0100

    s'accroupir en en tombat = vitesse + élevée

--- src/engine/physics.cpp
+++ src/engine/physics.cpp
@@ -1755,7 +1755,7 @@ void modifyvelocity(physent *pl, bool local, bool water, bool floating, int curt
         {
             if(pl==player) d.mul(floatspeed/100.0f);
         }
-        else if(pl->crouching) d.mul(0.4f);
+        else if(pl->crouching) pl->physstate==PHYS_FALL ? d.mul(2.f) : d.mul(aptitude==APT_NINJA ? 0.8f : aptitude==APT_COMMANDO ? 0.7f : 0.5f);
     }
     float fric = water && !floating ? 20.0f : (pl->physstate >= PHYS_SLOPE || floating ? 6.0f : 30.0f);
     pl->vel.lerp(d, pl->vel, pow(1 - 1/fric, curtime/20.0f));
