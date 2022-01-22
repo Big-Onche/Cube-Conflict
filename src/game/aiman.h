c40103d56d353d24c31a28e056da43706112cafd
CubeConflict <vod-k@live.be>
2022-01-22 16:10:51 +0100

    nouvelles répliques

--- src/game/aiman.h
+++ src/game/aiman.h
@@ -124,7 +124,7 @@ namespace aiman
         ci->level = skill;
         ci->customcape = rnd(14);
         ci->customtombe = rnd(11);
-        ci->customdanse = rnd(6);
+        ci->customdanse = rnd(7);
         ci->aireinit = 2;
         ci->connected = true;
 
--- src/game/game.cpp
+++ src/game/game.cpp
@@ -561,7 +561,7 @@ namespace game
         //return !((gameent *)d)->lasttaunt || lastmillis-((gameent *)d)->lasttaunt>=5000;
     }
 
-    VARFP(player1_danse, 0, 0, 6,
+    VARFP(player1_danse, 0, 0, 7,
     {
         if(stat[VOI_CORTEX+player1_danse]<= 0) {conoutf(CON_GAMEINFO, "\f3Vous ne possédez pas cette voix !"); playsound(S_ERROR); return;}
         addmsg(N_SENDDANSE, "ri", player1_danse);
