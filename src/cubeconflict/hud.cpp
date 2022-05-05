0feeec1d2bcb7851f7421265675b893cff3f7f9e
CubeConflict <vod-k@live.be>
2022-05-05 17:22:51 +0200

    bon affichage du mana avec sorts et kamikaze

--- src/cubeconflict/hud.cpp
+++ src/cubeconflict/hud.cpp
@@ -265,10 +265,15 @@ namespace game
 
         if(player1->aptitude==APT_MAGICIEN || player1->aptitude==APT_PHYSICIEN || player1->aptitude==APT_PRETRE || player1->aptitude==APT_SHOSHONE || player1->aptitude==APT_ESPION || player1->aptitude==APT_KAMIKAZE)
         {
-            if(player1->aptitude!=APT_KAMIKAZE) settexture("media/interface/hud/mana.png");
-            else if(player1->aptitude==APT_KAMIKAZE && player1->aptisort2) settexture("media/interface/hud/chrono.png");
-            if(player1->aptisort2)
+            if(player1->aptisort2 && player1->aptitude==APT_KAMIKAZE && player1->ammo[GUN_KAMIKAZE]>0)
             {
+                settexture("media/interface/hud/chrono.png");
+                bgquad(15, h-260, 115, 115);
+                decal_icon += 130;
+            }
+            else if(player1->aptitude!=APT_KAMIKAZE)
+            {
+                settexture("media/interface/hud/mana.png");
                 bgquad(15, h-260, 115, 115);
                 decal_icon += 130;
             }
@@ -372,8 +377,8 @@ namespace game
         if(d->armour > 0) draw_textf("%d", 370, h-103, d->armour < 9 ? 1 : d->armour/10);
 
 
-        if(player1->aptitude==APT_MAGICIEN || player1->aptitude==APT_PHYSICIEN || player1->aptitude==APT_PRETRE || player1->aptitude==APT_SHOSHONE || player1->aptitude==APT_ESPION || (player1->aptitude==APT_KAMIKAZE && player1->aptisort2>0))
-           {draw_textf("%d", 135, h-233-decal_number, player1->aptitude==APT_KAMIKAZE && player1->aptisort2>0 ? player1->aptisort2/1000 : player1->mana); decal_number +=130;}
+        if(player1->aptitude==APT_MAGICIEN || player1->aptitude==APT_PHYSICIEN || player1->aptitude==APT_PRETRE || player1->aptitude==APT_SHOSHONE || player1->aptitude==APT_ESPION || (player1->aptitude==APT_KAMIKAZE && player1->aptisort2>0 && player1->ammo[GUN_KAMIKAZE]>0))
+           {draw_textf("%d", 135, h-233-decal_number, player1->aptitude==APT_KAMIKAZE ? (player1->aptisort2-1500)/1000 : player1->mana); decal_number +=130;}
 
         if(player1->crouching && player1->aptitude==9) decal_number +=130;
         if(player1->ragemillis) {draw_textf("%d", 135, h-233-decal_number, d->ragemillis/1000); decal_number +=130;}
