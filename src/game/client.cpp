e616d6154b33d6417616cf76eb9d1e53248aa1f5
CubeConflict <vod-k@live.be>
2022-05-05 16:45:53 +0200

    son mono de changement d'arme en 1ere personne

--- src/game/client.cpp
+++ src/game/client.cpp
@@ -1991,7 +1991,7 @@ namespace game
                 int gun = getint(p);
                 if(!validgun(gun)) return;
                 d->gunselect = gun;
-                playsound(S_WEAPLOAD, &d->o);
+                playsound(S_WEAPLOAD, d==hudplayer() ? NULL : &d->o);
                 break;
             }
 
