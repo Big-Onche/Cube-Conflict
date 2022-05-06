d5ab7a214fbc1e64a111f0d2fd32fda6bb68ca7a
CubeConflict <vod-k@live.be>
2022-05-06 12:42:10 +0200

    adaptations dégats suite au fix

--- src/game/game.cpp
+++ src/game/game.cpp
@@ -655,6 +655,7 @@ namespace game
         }
 
         damageeffect(damage, d, actor, d!=h, atk);
+        if(d->aptitude==APT_VAMPIRE) regeneffect(damage, d, actor);
 
         ai::damaged(d, actor);
 
