6d6e540a0a0f8b529d90c8115ed1aca57d4f67b2
CubeConflict <vod-k@live.be>
2022-05-11 16:10:44 +0200

    modifs ecran de dommages

--- src/engine/rendergl.cpp
+++ src/engine/rendergl.cpp
@@ -2587,10 +2587,10 @@ int damageblendmillis = 0;
 
 VARFP(damagescreen, 0, 1, 1, { if(!damagescreen) damageblendmillis = 0; });
 VARP(damagescreenfactor, 1, 5, 100);
-VARP(damagescreenalpha, 1, 60, 100);
+VARP(damagescreenalpha, 1, 80, 100);
 VARP(damagescreenfade, 0, 1000, 1000);
 VARP(damagescreenmin, 1, 10, 1000);
-VARP(damagescreenmax, 1, 100, 1000);
+VARP(damagescreenmax, 1, 500, 1000);
 
 void damageblend(int n)
 {
