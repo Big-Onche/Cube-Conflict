ea25af74f1012f36af5d7da373f771c1f74d3825
CubeConflict <vod-k@live.be>
2022-12-16 17:30:15 +0100

    updated tutorial's code

--- src/engine/rendertext.cpp
+++ src/engine/rendertext.cpp
@@ -282,6 +282,7 @@ static void text_color(char c, char *stack, int size, int &sp, bvec color, int a
             case 'h': color = bvec(0,   200,   0); break;  // Menu green
             case 'i': color = bvec(255,   0, 255); break;  // Super violet
             case 'j': color = bvec(140, 180,   0); break;  // Yellow green
+            case 'k': color = bvec(32,   32,  32); break;  // Dark gray
             default: gle::color(color, a); return;         // provided color: everything else
         }
         if(textbright != 100) color.scale(textbright, 100);
