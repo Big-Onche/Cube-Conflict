66c4df0b8587abb66b9a3db743f78d6ff3c462ed
CubeConflict <vod-k@live.be>
2023-06-30 21:19:55 +0200

    fix | updated icons names

--- src/cubeconflict/stats.h
+++ src/cubeconflict/stats.h
@@ -31,12 +31,12 @@ static const struct statsinfo { const char *statname, *statnicenameFR, *statnice
     {"STAT_MORTS",              "Morts",                            "Deaths",                           "hud/grave.png"},
     {"STAT_KDRATIO",            "Ratio morts/éliminations",         "Kills/Deaths ratio",               "hud/stats.png"},       //Calculated in calcratio() with STAT_KILLS & STAT_MORTS then called in getstatinfo() STAT_KDRATIO not saved because float shit.
     {"STAT_DAMMAGERECORD",      "Record de dommages en une partie", "Damage record in a single match",  "hud/checkbox_on.jpg"},
-    {"STAT_KILLSTREAK",         "Meilleure série d'éliminations",   "Best killstreak",                  "hud/rage.png"},
+    {"STAT_KILLSTREAK",         "Meilleure série d'éliminations",   "Best killstreak",                  "hud/b_rage.png"},
     {"STAT_MAXKILLDIST",        "Elimination la plus éloignée",     "Farthest frag",                    "hud/campeur.png"},
     {"STAT_WINS",               "Victoires",                        "Victories",                        "hud/cool.jpg"},
     {"STAT_SUICIDES",           "Suicides",                         "Suicides",                         "hud/fou.jpg"},             //10
     {"STAT_ALLIESTUES",         "Alliés tués",                      "Killed allies",                    "hud/sournois_red.jpg"},
-    {"STAT_TIMEPLAYED",         "Temps de jeu",                     "Time played",                      "hud/chrono.png"},      //Calculated in secs with STAT_TIMEPLAYED in dotime(), calculated in HH:MM:SS for display in getstatinfo()
+    {"STAT_TIMEPLAYED",         "Temps de jeu",                     "Time played",                      "hud/timer.png"},      //Calculated in secs with STAT_TIMEPLAYED in dotime(), calculated in HH:MM:SS for display in getstatinfo()
     {"STAT_DRAPEAUXENVOL",      "Drapeaux ennemis volés",           "Stolen enemy flags",               "hud/drapeau_ennemi.png"},
     {"STAT_DRAPEAUXENRAP",      "Drapeaux ennemis remportés",       "Enemy flags won",                  "hud/drapeau_ennemi.png"},
     {"STAT_DRAPEAUXALYREC",     "Drapeaux alliés récupérés",        "Allied flags recovered",           "hud/drapeau_allie.png"},
@@ -44,25 +44,25 @@ static const struct statsinfo { const char *statname, *statnicenameFR, *statnice
     //Classes
     {"STAT_ABILITES",           "Abilitées utilisées",              "Used abilities",                   "hud/stats.png"},
     {"STAT_HEALTHREGEN",        "Santé redonnée aux alliés",        "Health restored to allies",        "apt_logo/1.png"},
-    {"STAT_HEALTHREGAIN",       "Santé récupérée grâce aux médecins", "Health recovered with medics",   "hud/coeur.png"},
+    {"STAT_HEALTHREGAIN",       "Santé récupérée grâce aux médecins", "Health recovered with medics",   "hud/health.png"},
     {"STAT_MANAREGEN",          "Mana redonné aux alliés",          "Mana restored to allies",          "hud/mana.png"},
     {"STAT_MANAREGAIN",         "Mana récupéré grâce aux junkies",  "Mana recovered with junkies",      "apt_logo/13.png"},           //20
     //Shields
-    {"STAT_BOUCLIERBOIS",       "Boucliers en bois utilisés",       "Wooden shields used",              "hud/bouclier_bois.png"},
-    {"STAT_BOUCLIERFER",        "Boucliers en fer utilisés",        "Iron shields used",                "hud/bouclier_fer.png"},
-    {"STAT_BOUCLIEROR",         "Boucliers en or utilisés",         "Gold shields used",                "hud/bouclier_or.png"},
-    {"STAT_BOUCLIERMAGNETIQUE", "Boucliers magnétiques utilisés",   "Magnetic shields used",            "hud/bouclier_magnetique.png"},
-    {"STAT_ARMUREASSIST",       "Armures assistées utilisés",       "Power armors used",                "hud/robot.png"},
+    {"STAT_BOUCLIERBOIS",       "Boucliers en bois utilisés",       "Wooden shields used",              "hud/s_wood.png"},
+    {"STAT_BOUCLIERFER",        "Boucliers en fer utilisés",        "Iron shields used",                "hud/s_iron.png"},
+    {"STAT_BOUCLIEROR",         "Boucliers en or utilisés",         "Gold shields used",                "hud/s_gold.png"},
+    {"STAT_BOUCLIERMAGNETIQUE", "Boucliers magnétiques utilisés",   "Magnetic shields used",            "hud/s_magnet.png"},
+    {"STAT_ARMUREASSIST",       "Armures assistées utilisés",       "Power armors used",                "hud/s_power.png"},
     {"STAT_REPASSIST",          "Réparations d'armure assistée",    "Power armor repairs",              "hud/options.jpg"},
     //Objects
-    {"STAT_PANACHAY",           "Panachays consommés",              "Beers drunk",                      "hud/coeur.png"},
+    {"STAT_PANACHAY",           "Panachays consommés",              "Beers drunk",                      "hud/health.png"},
     {"STAT_MANA",               "Potions de mana consommées",       "Mana potions consumed",            "hud/mana.png"},
     {"STAT_COCHON",             "Cochons grillés mangés",           "Grilled pigs eaten",               "hud/stats.png"},
-    {"STAT_STEROS",             "Cures de stéroïdes",               "Steroids cycles",                  "hud/steros.png"},           //30
-    {"STAT_EPO",                "Piqures d'EPO",                    "EPO shots",                        "hud/epo.png"},
-    {"STAT_JOINT",              "Joints fumés",                     "Smoked joints",                    "hud/joint.png"},
-    {"STAT_CHAMPIS",            "Champignons mangés",               "Shrooms eaten",                    "hud/champis.png"},
-    {"STAT_ARMES",              "Armes ramassées",                  "Picked up weapons",                "hud/chargeur.png"},
+    {"STAT_STEROS",             "Cures de stéroïdes",               "Steroids cycles",                  "hud/b_roids.png"},           //30
+    {"STAT_EPO",                "Piqures d'EPO",                    "EPO shots",                        "hud/b_epo.png"},
+    {"STAT_JOINT",              "Joints fumés",                     "Smoked joints",                    "hud/b_joint.png"},
+    {"STAT_CHAMPIS",            "Champignons mangés",               "Shrooms eaten",                    "hud/b_shrooms.png"},
+    {"STAT_ARMES",              "Armes ramassées",                  "Picked up weapons",                "hud/loader.png"},
     {"STAT_SUPERARMES",         "Super-caisses ramassées",          "Picked Up Super Crates",           "hud/stats.png"},
     //Stupid statistics
     {"STAT_ATOM",               "Bombes atomiques tirées",          "Amount of atom bomb fired",        "hud/stats.png"},
