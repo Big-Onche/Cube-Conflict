09c9497603997b3e2039d3f7dd5a8a2884dca290
CubeConflict <vod-k@live.be>
2024-01-20 18:21:10 +0100

    misc | shop code improvements

--- src/cubeconflict/customs.cpp
+++ src/cubeconflict/customs.cpp
@@ -4,9 +4,84 @@
 #include "stats.h"
 #include "customs.h"
 
+enum {CUST_SMILEY = 0, CUST_CAPE, CUST_GRAVE, NUMCUSTS};
+
 int smiley[NUMSMILEYS];
 int cape[NUMCAPES];
 int grave[NUMGRAVES];
+
+bool validItem(int type, int num)
+{
+    switch(type)
+    {
+        case CUST_SMILEY: return num >= 0 && num < NUMSMILEYS;
+        case CUST_CAPE: return num >= 0 && num < NUMCAPES;
+        case CUST_GRAVE: return num >= 0 && num < NUMGRAVES;
+    }
+    return false;
+}
+
+int itemPrice(int type, int num)
+{
+    switch(type)
+    {
+        case CUST_SMILEY: return customsmileys[num].price;
+        case CUST_CAPE: return customscapes[num].price;
+        case CUST_GRAVE: return customstombes[num].price;
+    }
+    return 0;
+}
+ICOMMAND(getitemprice, "ii", (int *itemType, int *itemNum), intret(itemPrice(*itemType, *itemNum)));
+
+bool hasItem(int type, int num)
+{
+    switch(type)
+    {
+        case CUST_SMILEY: return smiley[num];
+        case CUST_CAPE: return cape[num];
+        case CUST_GRAVE: return grave[num];
+    }
+    return false;
+}
+ICOMMAND(hasitem, "ii", (int *itemtype, int *itemnum), intret(hasItem(*itemtype, *itemnum)));
+
+const char* tooExpensive[NUMCUSTS] = {"Console_Shop_SmileyTooExpensive", "Console_Shop_CapeTooExpensive", "Console_Shop_GraveTooExpensive" };
+const char* alreadyOwned[NUMCUSTS] = {"Console_Shop_SmileyOwned", "Console_Shop_CapeOwned", "Console_Shop_GraveOwned" };
+const char* itemBuyed[NUMCUSTS] = {"Console_Shop_SmileyBuyed", "Console_Shop_CapeBuyed", "Console_Shop_GraveBuyed" };
+
+void buyItem(int type, int num)
+{
+    if(!validItem(type, num)) return;
+
+    if(itemPrice(type, num) > stat[STAT_CC]) // no enough money
+    {
+        conoutf(CON_GAMEINFO, "\f3%s", readstr(tooExpensive[type]));
+        playSound(S_ERROR);
+        return;
+    }
+    else if(hasItem(type, num)) // item already owned
+    {
+        conoutf(CON_GAMEINFO, "\f3%s", readstr(alreadyOwned[type]));
+        playSound(S_ERROR);
+    }
+    else // take the money
+    {
+        stat[STAT_CC] -= itemPrice(type, num);
+
+        switch(type)
+        {
+            case CUST_SMILEY: smiley[num] = max(rnd(256), 1);break;
+            case CUST_CAPE: cape[num] = max(rnd(256), 1); break;
+            case CUST_GRAVE: grave[num] = max(rnd(256), 1);
+        }
+
+        playSound(S_CAISSEENREGISTREUSE);
+        conoutf(CON_GAMEINFO, "\fe%s", readstr(itemBuyed[type]));
+        unlockAchievement(ACH_TMMONEY);
+    }
+}
+
+ICOMMAND(buyitem, "ii", (int *itemType, int *itemNum), buyItem(*itemType, *itemNum));
 
 ICOMMAND(getnumsmileys, "", (), intret(NUMSMILEYS));
 ICOMMAND(getnumcapes, "", (), intret(NUMCAPES));
@@ -25,71 +100,3 @@ const char *getgravedir(int grave)
     sprintf(dir, "graves/%s", customstombes[grave].ident);
     return dir;
 }
-
-enum {CUST_SMILEY = 0, CUST_CAPE, CUST_GRAVE};
-
-int itemprice(int itemtype, int itemnum)
-{
-    switch(itemtype)
-    {
-        case CUST_SMILEY: return customsmileys[itemnum].price;
-        case CUST_CAPE: return customscapes[itemnum].price;
-        case CUST_GRAVE: return customstombes[itemnum].price;
-    }
-    return 0;
-}
-ICOMMAND(getitemprice, "ii", (int *itemtype, int *itemnum), intret(itemprice(*itemtype, *itemnum)));
-
-int hasitem(int itemtype, int itemnum)
-{
-    switch(itemtype)
-    {
-        case CUST_SMILEY: return smiley[itemnum];
-        case CUST_CAPE: return cape[itemnum];
-        case CUST_GRAVE: return grave[itemnum];
-    }
-    return 0;
-}
-ICOMMAND(hasitem, "ii", (int *itemtype, int *itemnum), intret(hasitem(*itemtype, *itemnum)));
-
-void buyitem(int itemtype, int itemnum)
-{
-    switch(itemtype)
-    {
-        case CUST_SMILEY:
-            if(customsmileys[itemnum].price > stat[STAT_CC]) {conoutf(CON_GAMEINFO, "\f3%s", readstr("Console_Shop_SmileyTooExpensive")); playSound(S_ERROR); return; }
-            else if(smiley[itemnum]) {conoutf(CON_GAMEINFO, "\f3%s", readstr("Console_Shop_SmileyOwned"));  playSound(S_ERROR); return; }
-            else
-            {
-                stat[STAT_CC] -= customsmileys[itemnum].price;
-                smiley[itemnum] = max(rnd(256), 1);
-                playSound(S_CAISSEENREGISTREUSE);
-                conoutf(CON_GAMEINFO, "\fe%s", readstr("Console_Shop_SmileyBuyed"));
-            }
-            break;
-        case CUST_CAPE:
-            if(customscapes[itemnum].price > stat[STAT_CC]) {conoutf(CON_GAMEINFO, "\f3%s", readstr("Console_Shop_CapeTooExpensive")); playSound(S_ERROR); return; }
-            else if(cape[itemnum]) {conoutf(CON_GAMEINFO, "\f3%s", readstr("Console_Shop_CapeOwned")); playSound(S_ERROR); return; }
-            else
-            {
-                stat[STAT_CC] -= customscapes[itemnum].price;
-                cape[itemnum] = max(rnd(256), 1);
-                playSound(S_CAISSEENREGISTREUSE);
-                conoutf(CON_GAMEINFO, "\fe%s", readstr("Console_Shop_CapeBuyed"));
-            }
-            break;
-        case CUST_GRAVE:
-            if(customstombes[itemnum].price > stat[STAT_CC]) {conoutf(CON_GAMEINFO, "\f3%s", readstr("Console_Shop_GraveTooExpensive")); playSound(S_ERROR); return; }
-            else if(grave[itemnum]) {conoutf(CON_GAMEINFO, "\f3%s", readstr("Console_Shop_GraveOwned")); playSound(S_ERROR); return; }
-            else
-            {
-                stat[STAT_CC] -= customstombes[itemnum].price;
-                grave[itemnum] = max(rnd(256), 1);
-                playSound(S_CAISSEENREGISTREUSE);
-                conoutf(CON_GAMEINFO, "\fe%s", readstr("Console_Shop_GraveBuyed"));
-            }
-            break;
-    }
-    unlockAchievement(ACH_TMMONEY);
-}
-ICOMMAND(buyitem, "ii", (int *itemtype, int *itemnum), buyitem(*itemtype, *itemnum));
