b5df0bf9b0e4f4513e92a15fa43c9f6e24bb2e8a
CubeConflict <vod-k@live.be>
2024-03-16 15:41:56 +0100

    game | working on new customization unlocking

--- src/cubeconflict/customs.cpp
+++ src/cubeconflict/customs.cpp
@@ -26,8 +26,8 @@ int itemPrice(int type, int num)
     switch(type)
     {
         case CUST_SMILEY: return customsmileys[num].price;
-        case CUST_CAPE: return customscapes[num].price;
-        case CUST_GRAVE: return customstombes[num].price;
+        case CUST_CAPE: return capes[num].price;
+        case CUST_GRAVE: return graves[num].price;
     }
     return 0;
 }
@@ -45,6 +45,121 @@ bool hasItem(int type, int num)
 }
 ICOMMAND(hasitem, "ii", (int *itemtype, int *itemnum), intret(hasItem(*itemtype, *itemnum)));
 
+int rollItemRarity()
+{
+    int roll = rnd(100); // determine the rarity based on rolloff probabilities
+
+    if(roll < 50) return D_COMMON;
+    else if(roll < 75) return D_UNCOMMON;
+    else if(roll < 90) return D_RARE;
+    else if(roll < 98) return D_LEGENDARY;
+    else return D_GODLY;
+}
+
+void unlockRandomItem()
+{
+    int rarity = rollItemRarity();
+    bool unlockItem = rnd(2);
+
+    if(unlockItem)
+    {
+        int itemType = rnd(NUMCUSTS);
+        int itemNum;
+
+        switch(itemType)
+        {
+            case CUST_SMILEY: itemNum = NUMSMILEYS; break;
+            case CUST_CAPE: itemNum = NUMCAPES; break;
+            case CUST_GRAVE: itemNum = NUMGRAVES; break;
+            default: return;
+        }
+
+        bool itemUnlocked = false;
+        while(!itemUnlocked)
+        {
+            int count = 0;
+            loopi(itemNum)
+            {
+                switch(itemType)
+                {
+                    case CUST_SMILEY: if(customsmileys[i].value == rarity && !hasItem(itemType, i)) count++; break;
+                    case CUST_CAPE: if(capes[i].value == rarity && !hasItem(itemType, i)) count++; break;
+                    case CUST_GRAVE: if(graves[i].value == rarity && !hasItem(itemType, i)) count++; break;
+                }
+            }
+
+            if(count > 0) // ensure there is at least one item of the selected rarity
+            {
+                int randomSelection = rnd(count);
+                for(int i = 0, found = 0; i < itemNum && found <= randomSelection; i++)
+                {
+                    switch(itemType)
+                    {
+                        case CUST_SMILEY:
+                            if(customsmileys[i].value == rarity && !hasItem(itemType, i))
+                            {
+                                if(found == randomSelection)
+                                {
+                                    conoutf("You've unlocked a %s smiley!", customsmileys[i].ident);
+                                    itemUnlocked = true; // exit the while loop
+                                    break; // exit the for loop
+                                }
+                                found++;
+                            }
+                            break;
+
+                        case CUST_CAPE:
+                            if(capes[i].value == rarity && !hasItem(itemType, i))
+                            {
+                                if(found == randomSelection)
+                                {
+                                    conoutf("You've unlocked a %s cape!", capes[i].name);
+                                    itemUnlocked = true; // exit the while loop
+                                    break; // exit the for loop
+                                }
+                                found++;
+                            }
+                            break;
+
+                        case CUST_GRAVE:
+                            if(graves[i].value == rarity && !hasItem(itemType, i))
+                            {
+                                if(found == randomSelection)
+                                {
+                                    conoutf("You've unlocked a %s grave!", graves[i].name);
+                                    itemUnlocked = true; // exit the while loop
+                                    break; // exit the for loop
+                                }
+                                found++;
+                            }
+                            break;
+                    }
+                }
+            }
+            else
+            {
+                conoutf("No more items (%d) of this rarity (%d) to unlock.", itemType, rarity);
+                unlockItem = false;
+                break; // exit the while loop if no items are available to unlock
+            }
+        }
+    }
+
+    if(!unlockItem)
+    {
+        int ccoAmount;
+        switch(rarity)
+        {
+            case D_COMMON: ccoAmount = 3; break;
+            case D_UNCOMMON: ccoAmount = 5; break;
+            case D_RARE: ccoAmount = 10; break;
+            case D_LEGENDARY: ccoAmount = 15; break;
+            case D_GODLY: ccoAmount = 30;
+        }
+        conoutf("You've been awarded %d Golden Cisla Coins!", ccoAmount);
+    }
+}
+
 const char* tooExpensive[NUMCUSTS] = {"Console_Shop_SmileyTooExpensive", "Console_Shop_CapeTooExpensive", "Console_Shop_GraveTooExpensive" };
 const char* alreadyOwned[NUMCUSTS] = {"Console_Shop_SmileyOwned", "Console_Shop_CapeOwned", "Console_Shop_GraveOwned" };
 const char* itemBuyed[NUMCUSTS] = {"Console_Shop_SmileyBuyed", "Console_Shop_CapeBuyed", "Console_Shop_GraveBuyed" };
@@ -56,13 +171,13 @@ void buyItem(int type, int num)
     if(itemPrice(type, num) > stat[STAT_CC]) // no enough money
     {
         conoutf(CON_GAMEINFO, "\f3%s", readstr(tooExpensive[type]));
-        playSound(S_ERROR);
+        playSound(S_ERROR, vec(0, 0, 0), 0, 0, SND_UI);
         return;
     }
     else if(hasItem(type, num)) // item already owned
     {
         conoutf(CON_GAMEINFO, "\f3%s", readstr(alreadyOwned[type]));
-        playSound(S_ERROR);
+        playSound(S_ERROR, vec(0, 0, 0), 0, 0, SND_UI);
     }
     else // take the money
     {
@@ -86,17 +201,3 @@ ICOMMAND(buyitem, "ii", (int *itemType, int *itemNum), buyItem(*itemType, *itemN
 ICOMMAND(getnumsmileys, "", (), intret(NUMSMILEYS));
 ICOMMAND(getnumcapes, "", (), intret(NUMCAPES));
 ICOMMAND(getnumgraves, "", (), intret(NUMGRAVES));
-
-const char *getcapedir(int cape, bool enemy)
-{
-    static char dir[64];
-    sprintf(dir, "capes/%s%s", customscapes[cape].ident, enemy ? "/enemy" : "");
-    return dir;
-}
-
-const char *getgravedir(int grave)
-{
-    static char dir[64];
-    sprintf(dir, "graves/%s", customstombes[grave].ident);
-    return dir;
-}
