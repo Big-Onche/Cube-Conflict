f244817632fd0ab8ecf6060685615aa37e02658b
CubeConflict <vod-k@live.be>
2024-04-03 17:59:33 +0200

    ai | trying to make them less performance expensive while avoiding making them stupid

--- src/game/ai.cpp
+++ src/game/ai.cpp
@@ -260,7 +260,7 @@ namespace ai
                 itermillis = totalmillis;
             }
             int count = 0;
-            loopv(players) if(players[i]->ai) think(players[i], ++count == iteration ? true : false);
+            loopv(players) if(players[i]->ai) think(players[i], ++count == iteration && (totalmillis % 150 < 75) ? true : false);
             if(++iteration > count) iteration = 0;
         }
     }
@@ -532,7 +532,7 @@ namespace ai
                 break;
             default:
             {
-                if(e.type >= I_RAIL && e.type <= I_GLOCK && !d->hasmaxammo(e.type)/2 && !m_noammo)
+                if(e.type >= I_RAIL && e.type <= I_GLOCK && !d->hasmaxammo(e.type) && !m_noammo)
                 {
                     int gun = e.type - I_RAIL + GUN_ELEC;
                     if(isgoodammo(gun)) score = hasgoodammo(d) ? m_ctf ? 1e1f : 1e2f : m_ctf ? 1e2f : 1e4f;
@@ -1406,7 +1406,7 @@ namespace ai
             {
                 if(d->ragdoll) cleanragdoll(d);
                 moveplayer(d, 10, true, d->boostmillis[B_EPO], d->boostmillis[B_JOINT], d->aptitude, d->aptitude==APT_MAGICIEN ? d->abilitymillis[ABILITY_1] : d->aptitude==APT_SHOSHONE || d->aptitude==APT_ESPION || d->aptitude==APT_KAMIKAZE ? d->abilitymillis[ABILITY_2] : d->abilitymillis[ABILITY_3], d->armourtype==A_ASSIST && d->armour ? true : false);
-                if(allowmove && !b.idle) timeouts(d, b);
+                if(allowmove && !b.idle && run) timeouts(d, b);
                 entities::checkitems(d);
                 if(cmode) cmode->checkitems(d);
             }
