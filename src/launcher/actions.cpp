fb6efd5ac38263fbee154f7dfa3e89b7fcb5a4ad
CubeConflict <vod-k@live.be>
2024-04-04 16:03:43 +0200

    fix | game launcher args

--- src/launcher/actions.cpp
+++ src/launcher/actions.cpp
@@ -58,12 +58,16 @@ namespace action
         }
         else if(pid == 0) // Child process, execute the game binary
         {
-            char* clientArgs[] = {const_cast<char*>(gamePath().c_str()), const_cast<char*>("-u$HOME/.cubeconflict"), const_cast<char*>(logsArg().c_str()), const_cast<char*>(langArg().c_str()), const_cast<char*>(steamArg().c_str()), nullptr};
+            std::vector<std::string> args = {gamePath(), "-u$HOME/.cubeconflict", langArg()};
+            std::vector<char*> clientArgs;
+            for (const auto& arg : args) clientArgs.push_back(const_cast<char*>(arg.c_str()));
+            if(isUsingSteam) clientArgs.push_back(const_cast<char*>("-s"));
+            clientArgs.push_back(nullptr); // Add a null terminator
 
             std::string serverExec = gamePath() + " -d";
     		char* serverArgs[] = {const_cast<char*>("xterm"), const_cast<char*>("-e"), const_cast<char*>(serverExec.c_str()), nullptr};
 
-    		if(dedicatedServer ? (execvp(serverArgs[0], serverArgs) == -1) : (execvp(clientArgs[0], clientArgs) == -1))
+            if(dedicatedServer ? (execvp(serverArgs[0], serverArgs) == -1) : (execvp(clientArgs[0], clientArgs.data()) == -1))
             {
                 if(dedicatedServer) error::pop(getString("Error_Title").c_str(), getString("Error_Unix_Exec_Serv").c_str());
                 else error::pop(getString("Error_Title").c_str(), getString("Error_Unix_Exec").c_str());
